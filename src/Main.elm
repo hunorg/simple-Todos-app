1a47e30a57cd05b1eaa3cfcc2589271437d82f5f
hunorgered <114682020+hunorg@users.noreply.github.com>
2023-02-07 02:49:37 +0100

    Add files via upload

---
+++ src/Main.elm
@@ -0,0 +1,197 @@
+module Main exposing (..)
+
+import Array
+import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import String exposing (dropRight)
+
+
+type alias Model =
+    { todos : List Todo
+    , newTodo : String
+    , selectedTodo : Maybe Todo
+    }
+
+
+type alias Todo =
+    { description : String
+    , completed : Bool
+    , editing : Bool
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { todos = []
+      , newTodo = ""
+      , selectedTodo = Nothing
+      }
+    , Cmd.none
+    )
+
+
+type Msg
+    = AddTodo
+    | UpdateNewTodo String
+    | SelectTodo Int
+    | ToggleTodo
+    | UpdateSelectedTodo Todo
+    | EditTodo Int
+    | RemoveTodo
+    | RemoveCompleted
+
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        AddTodo ->
+            ( { model
+                | todos = model.todos ++ [ { description = model.newTodo, completed = False, editing = False } ]
+                , newTodo = ""
+                , selectedTodo = Nothing
+              }
+            , Cmd.none
+            )
+
+        UpdateNewTodo newDescription ->
+            ( { model | newTodo = newDescription }, Cmd.none )
+
+        SelectTodo index ->
+            if model.selectedTodo == Nothing then 
+
+                ( { model | selectedTodo = Array.get index (Array.fromList model.todos) }, Cmd.none )
+            else 
+                ( { model | selectedTodo = Nothing }, Cmd.none )
+
+        ToggleTodo ->
+            case model.selectedTodo of
+                Just todo ->
+                    let
+                        newTodo =
+                            if todo.completed then 
+                                { todo | description = dropRight 12 todo.description, completed = False }
+                            else 
+                                 { todo | description = todo.description ++ " (completed)", completed = True }
+
+                        newTodos =
+                            List.map
+                                (\t ->
+                                    if t == todo then
+                                        newTodo
+
+                                    else
+                                        t
+                                )
+                                model.todos
+                    in
+                    ( { model | todos = newTodos, selectedTodo = Just newTodo }, Cmd.none )
+
+                Nothing ->
+                    ( model, Cmd.none )
+
+        UpdateSelectedTodo updatedTodo ->
+            let
+                newTodos =
+                    List.map
+                        (\t ->
+                            if t == Maybe.withDefault { description = "", completed = False, editing = False } model.selectedTodo then
+                                updatedTodo
+
+                            else
+                                t
+                        )
+                        model.todos
+            in
+            ( { model | todos = newTodos, selectedTodo = Just updatedTodo }, Cmd.none )
+
+        EditTodo index ->
+            let
+                selectedTodo =
+                    Array.get index (Array.fromList model.todos)
+
+                updatedTodo =
+                    Maybe.withDefault { description = "", completed = False, editing = False } selectedTodo
+
+                newTodos =
+                    List.map
+                        (\t ->
+                            if t == updatedTodo then
+                                { updatedTodo | editing = True }
+
+                            else
+                                t
+                        )
+                        model.todos
+            in
+            ( { model | todos = newTodos, selectedTodo = Just updatedTodo }, Cmd.none )
+
+        RemoveTodo ->
+            case model.selectedTodo of
+                Just todo ->
+                    let
+                        newTodos =
+                            List.filter (\t -> t /= todo) model.todos
+                    in
+                    ( { model | todos = newTodos, selectedTodo = Nothing }, Cmd.none )
+
+                Nothing ->
+                    ( model, Cmd.none )
+
+        RemoveCompleted -> 
+                        let 
+                            newTodos = 
+                                List.filter (\t -> not t.completed) model.todos
+                        in 
+                        ( { model | todos = newTodos }, Cmd.none)
+       
+
+                    
+
+
+view : Model -> Html Msg
+view model =
+    div [ ]
+        [ input [ value model.newTodo, onInput UpdateNewTodo ] []
+        , button [ onClick AddTodo ] [ text "Add Todo" ]
+        , ul []
+            (List.indexedMap
+                (\index todo ->
+                    li [ onClick (SelectTodo index) ]
+                        [ if todo.editing then
+                            div []
+                                [ input [ value todo.description, onInput (\newDescription -> UpdateSelectedTodo { todo | description = newDescription }) ] []
+                                , button [ onClick (UpdateSelectedTodo { todo | editing = False }) ] [ text "Save" ]
+                                ]
+
+                          else
+                            span []
+                                [ text todo.description
+                                , if model.selectedTodo == Just todo then
+                                    div []
+                                        [ button [ onClick (EditTodo index) ] [ text "Edit" ]
+                                        , button [ onClick ToggleTodo ] [ text "Completed" ]
+                                        , button [onClick RemoveTodo] [text "Remove"]
+                                        ]
+
+                                  else
+                                    text ""
+                                ]
+                        ]
+                )
+                model.todos
+            )
+        , button [onClick RemoveCompleted ] [text "RemoveCompleted"]
+        ]
+
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = \_ -> init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
